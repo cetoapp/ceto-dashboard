4f68c9a230f787afcd4ff16d738555ae57000405
GhaziJribi <ghazijribi@outlook.fr>
2026-01-09 22:37:45 +0100

    Adding Ceto Icons

---
+++ src/components/common/logo-box/icon-box.tsx
@@ -0,0 +1,20 @@
+import { IconAvatar } from "../icon-avatar";
+import CetoLogo from "../../../assets/images/white-logo.svg";
+
+type IconBoxProps = {
+  size?: "small" | "xlarge" | "large"
+}
+
+export default function IconBox({ size }: IconBoxProps) {
+  return (
+    <IconAvatar
+      size={size || "small"}
+      className="bg-ui-button-neutral shadow-buttons-neutral size-7 after:button-neutral-gradient relative flex items-center justify-center rounded-[6px] after:inset-0 after:content-['']"
+    >
+      <img
+        src={CetoLogo}
+        className="rounded-[6px] max-w-full h-auto"
+      />
+    </IconAvatar>
+  );
+}
